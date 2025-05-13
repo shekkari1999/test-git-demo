692e8e8e1db42cd1556c60319f291712b27b22e1
Akhil Shekkari <akhil.masters21@gmail.com>
2025-05-13 14:00:36 -0400

    AI suggestion applied to lang.cpp

--- lang.cpp
+++ lang.cpp
@@ -1,4 +1,2 @@
-ignoreUnregisteredUsers = settings->value(\"chat/ignore_unregistered\", false).toBool();
-attemptAutoConnect = settings->value(\"server/auto_connect\", 0).toInt() == 0 ? false : true; 
-}
-void SettingsCache::setLang(const QString &_lang)
+def add(a, b):
+return a * b
