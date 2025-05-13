1ca1c6ad35a4bb27c410125f49303d6933e6b182
Akhil Shekkari <akhil.masters21@gmail.com>
2025-05-13 13:54:47 -0400

    Update lang.cpp

--- lang.cpp
+++ lang.cpp
@@ -1,4 +1,5 @@
 ignoreUnregisteredUsers = settings->value(\"chat/ignore_unregistered\", false).toBool();
+{
 attemptAutoConnect = settings->value(\"server/auto_connect\", 0).toInt() == 0 ? false : true; 
 }
 void SettingsCache::setLang(const QString &_lang)
