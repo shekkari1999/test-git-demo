ebc89153056dac9aa0170c39358fd15e699ebe9c
Akhil Shekkari <akhil.masters21@gmail.com>
2025-05-13 14:42:30 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,3 +3,10 @@ def add(a, b):
   This funntion adds two numbers and returns the result
   '''
   return a - b
+
+def multiply(a, b):
+  '''
+  This funntion multiplies two numbers and returns the result
+  '''
+  return a * b
+  
